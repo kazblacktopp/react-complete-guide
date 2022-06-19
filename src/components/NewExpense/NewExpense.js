fa10bb45c3d6d3c988d2d4f4c95a16b881436a11
kazblacktopp <karen.blacktopp@gmail.com>
2022-06-19 14:21:13 +1000

    Added button for showing new expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,40 @@
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(props) {
+  const [isAddingNewExpense, setIsAddingNewExpense] = useState(false);
+
   function saveNewExpenseHandler(newExpenseData) {
     const expenseData = {
       ...newExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsAddingNewExpense(false);
+  }
+
+  function addNewExpenseHandler() {
+    setIsAddingNewExpense(true);
+  }
+
+  function cancelNewExpense() {
+    setIsAddingNewExpense(false);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveNewExpense={saveNewExpenseHandler} />
+      {!isAddingNewExpense && (
+        <button type="button" onClick={addNewExpenseHandler}>
+          Add New Expense
+        </button>
+      )}
+      {isAddingNewExpense && (
+        <ExpenseForm
+          onSaveNewExpense={saveNewExpenseHandler}
+          onCancel={cancelNewExpense}
+        />
+      )}
     </div>
   );
 }
