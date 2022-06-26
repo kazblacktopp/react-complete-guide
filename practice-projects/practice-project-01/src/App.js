1e8ab97b0e1c27924490fe7e5c5f8257df87a9c9
kazblacktopp <karen.blacktopp@gmail.com>
2022-06-26 12:25:36 +1000

    Added various components and states

--- practice-projects/practice-project-01/src/App.js
+++ practice-projects/practice-project-01/src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
-
+import { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import ErrorModal from './components/UI/ErrorModal';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const [error, setError] = useState();
+
+  function addUserHandler(newUsername, newUserAge) {
+    setUsersList(prevUsersList => {
+      const newUsersList = [...prevUsersList];
+      newUsersList.push({
+        username: newUsername,
+        age: +newUserAge,
+        id: `User${newUsersList.length + 1}`,
+      });
+      return newUsersList;
+    });
+  }
+
+  function onErrorHandler(errorDataObj) {
+    setError(errorDataObj);
+  }
+
+  function exitModalHandler() {
+    setError(null);
+  }
+
   return (
     <div>
-
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onClick={exitModalHandler}
+        />
+      )}
+      <div>
+        <AddUser onAddUser={addUserHandler} onError={onErrorHandler} />
+      </div>
+      {usersList.length > 0 && <UsersList users={usersList} />}
     </div>
   );
 }
