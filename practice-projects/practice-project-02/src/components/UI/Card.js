b9a7f36aab23e480caaea5bd7ca659f6e86db794
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-24 14:16:13 +1000

    Created Card module with styles

---
+++ practice-projects/practice-project-02/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import classes from './Card.module.css';
+
+function Card(props) {
+  const classNames = `${classes.card} ${props.className}`;
+
+  return <div className={classNames}>{props.children}</div>;
+}
+
+export default Card;
