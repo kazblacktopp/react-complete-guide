dfb2531e84a866acc746d83502acf3a6684e39d9
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-25 12:55:10 +1000

    Added cart modal handlers

--- practice-projects/practice-project-02/src/App.js
+++ practice-projects/practice-project-02/src/App.js
@@ -1,15 +1,28 @@
-import { Fragment } from 'react';
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+
+  function openCartHandler() {
+    setCartIsOpen(true);
+  }
+
+  function closeCartHandler() {
+    setCartIsOpen(false);
+  }
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsOpen && <Cart onCloseCart={closeCartHandler} />}
+      <Header onOpenCart={openCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
