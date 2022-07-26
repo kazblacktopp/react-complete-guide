4d9ac7d7c5fcc4572b5dc64426872783fad43361
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-27 08:40:20 +1000

    Added handler function for form submission

--- practice-projects/practice-project-02/src/components/Meals/MealItem/MealItemForm.js
+++ practice-projects/practice-project-02/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 function MealItemForm(props) {
+  const inputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+
+    const inputAmount = inputRef.current.value;
+    const inputAmountNumber = +inputAmount;
+
+    if (
+      inputAmount.trim() === '' ||
+      inputAmountNumber < 1 ||
+      inputAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onSubmitItem(inputAmountNumber);
+  }
+
   return (
-    <form className={classes.form}>
+    <form id={props.id} className={classes.form} onSubmit={formSubmitHandler}>
       <Input
+        ref={inputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
@@ -15,7 +38,10 @@ function MealItemForm(props) {
           defaultValue: '1',
         }}
       />
-      <button>+ Add</button>
+      <button form={props.id} type="submit">
+        + Add
+      </button>
+      {!amountIsValid && <p>Please enter a valid number between 1 and 5.</p>}
     </form>
   );
 }
