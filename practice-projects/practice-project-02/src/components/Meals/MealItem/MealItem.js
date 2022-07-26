073ee48482f5d39f0ee580ee73759c8fdf1036e8
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-27 08:41:19 +1000

    Added handler function for adding item using CartContext

--- practice-projects/practice-project-02/src/components/Meals/MealItem/MealItem.js
+++ practice-projects/practice-project-02/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
 
 function MealItem(props) {
   const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
+
+  function submitItemHandler(amount) {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: +amount,
+      price: props.price,
+    });
+  }
 
   return (
     <li className={classes.meal}>
@@ -12,7 +24,7 @@ function MealItem(props) {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onSubmitItem={submitItemHandler} />
       </div>
     </li>
   );
