ce31bb912838d64fd8d7ca228b79b0077ca07176
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-27 08:43:16 +1000

    Added state reducer for updating Cart items

--- practice-projects/practice-project-02/src/store/CartProvider.js
+++ practice-projects/practice-project-02/src/store/CartProvider.js
@@ -1,15 +1,57 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
-function CartProvider(props) {
-  function addToCartHandler(item) {}
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  function removeFromCartHandler(id) {}
+function cartReducer(state, action) {
+  if (action.type === 'ADD-ITEM') {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const exisitingCartItem = state.items[existingCartItemIndex];
+
+    let updatedCartItems;
+
+    if (exisitingCartItem) {
+      const updatedCartItem = { ...exisitingCartItem };
+      updatedCartItem.amount = exisitingCartItem.amount + action.item.amount;
+      updatedCartItems = [...state.items];
+      updatedCartItems[existingCartItemIndex] = updatedCartItem;
+    } else {
+      updatedCartItems = state.items.concat(action.item);
+    }
+
+    return { items: updatedCartItems, totalAmount: updatedTotalAmount };
+  }
+  return defaultCartState;
+}
+
+function CartProvider(props) {
+  const [cartState, dispatchCartActions] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  function addItemToCartHandler(item) {
+    dispatchCartActions({ type: 'ADD-ITEM', item: item });
+  }
+
+  function removeItemFromCartHandler(id) {
+    dispatchCartActions({ type: 'REMOVE-ITEM', id: id });
+  }
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
-    addItem: addToCartHandler,
-    removeItem: removeFromCartHandler,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
 
   return (
