7398c2ab5c0bd9f84f53d99b76de62a72d4b4242
kazblacktopp <karen.blacktopp@gmail.com>
2022-09-06 07:22:20 +1000

    Converted hard coded data to http request

--- http-request-project/src/App.js
+++ http-request-project/src/App.js
@@ -1,33 +1,37 @@
-import React from 'react';
-
+import { Fragment, useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films/')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const movieData = data.results.map(movieResult => {
+          return {
+            id: movieResult.episode_id,
+            title: movieResult.title,
+            releaseDate: movieResult.release_date,
+            openingText: movieResult.opening_crawl,
+          };
+        });
+        setMovies(movieData);
+      });
+  }
 
   return (
-    <React.Fragment>
+    <Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
